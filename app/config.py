33ca30726d9b70562ab3c76ebb013453a9d17922
JuanP <pablo.perez@i-vsp.com>
2026-02-26 12:44:25 -0600

    version para lectura de video con FFMPEG en vez de GStreamer

--- app/config.py
+++ app/config.py
@@ -10,5 +10,5 @@ CONF_THRESHOLD = 0.25
 
 TARGET_CLASSES = [0, 1, 2, 3, 5, 7]
 LINE_Y = 400
-VIDEO_PATH = '/app/app/traffic.mp4'
+VIDEO_PATH = "/app/app/traffic.mp4"
 # VIDEO_PATH = 0
--- app/services/camera.py
+++ app/services/camera.py
@@ -1,3 +1,24 @@
+# import cv2
+# from config import VIDEO_PATH
+
+
+# class CameraService:
+
+#     def __init__(self, source=VIDEO_PATH):
+#         self.cap = cv2.VideoCapture(source)
+
+#         if not self.cap.isOpened():
+#             raise RuntimeError("Cannot open camera")
+
+#     def read(self):
+#         ret, frame = self.cap.read()
+#         if not ret:
+#             return None
+#         return frame
+
+#     def release(self):
+#         self.cap.release()
+#------------------------------------ Para alternativa de video mp4
 import cv2
 from config import VIDEO_PATH
 
@@ -5,10 +26,11 @@ from config import VIDEO_PATH
 class CameraService:
 
     def __init__(self, source=VIDEO_PATH):
-        self.cap = cv2.VideoCapture(source)
+        # Forzamos backend FFMPEG en vez de GStreamer
+        self.cap = cv2.VideoCapture(source, cv2.CAP_FFMPEG)
 
         if not self.cap.isOpened():
-            raise RuntimeError("Cannot open camera")
+            raise RuntimeError(f"Cannot open video source: {source}")
 
     def read(self):
         ret, frame = self.cap.read()
